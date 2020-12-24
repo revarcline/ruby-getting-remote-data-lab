5d90566f20d0066aae55ed3e2c422d8437f153dd
Alex Cline <rev.a.r.cline@gmail.com>
2020-12-23 19:46:22 -0600

    commit tree a96dce6b6079d68f3961833125d218612b3c5edb

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,18 @@
-# Write your code here
+require 'json'
+require 'net/http'
+require 'open-uri'
+
+# the get requester has logged in
+class GetRequester
+  def initialize(url)
+    @url = url
+  end
+
+  def get_response_body
+    Net::HTTP.get_response(URI.parse(@url)).body
+  end
+
+  def parse_json
+    JSON.parse(get_response_body)
+  end
+end
